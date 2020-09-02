4acf8b0addbf2fdc01d0243b90c95f48813aedf8
AakankshaVats <68775539+AakankshaVats@users.noreply.github.com>
2020-09-02 21:13:22 +0530

    Delete g.cpp

--- g.cpp
+++
@@ -1,2 +0,0 @@
-/*Modify the Program-6 to separate the interface and implementation for the purpose of reusability. You
-should break the program into three different parts as discussed in lectures.*/
